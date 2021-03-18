522c3a9c9e76b9369ff11752604ba8fb078e757a
MuhammadQasim97 <qasimnazir99@gmail.com>
2021-03-18 12:57:05 +0500

    Create test_case.c

---
+++ download/hello-handout/test_case.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "hello.c"
+int main() {
+   // printf() displays the string inside quotation
+   int ans=testing();
+
+   if(ans==10)
+   {
+   		return 1;
+   }
+   else{
+   	return 0;
+   }
+}
